e6cab3ee6393ef8c1e5bc363c199fb0a88cba427
hal-da <daniel.halasz@gmx.net>
2022-11-30 21:47:17 +0100

    first commit

---
+++ day01/day01.js
@@ -0,0 +1,3 @@
+import { testData, testResult1,testResult2, realData } from "./day01data.js";
+
+console.log(testData, testResult1, testResult2, realData )
---
+++ day01/day01data.js
@@ -0,0 +1,4 @@
+export const testData = 'test'
+export const testResult1 = 'test result 1'
+export const testResult2 = `test result 2`
+export const realData = `realData`
