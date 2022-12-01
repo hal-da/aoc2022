d0ab29761f3bd7f76459f2c61deb9cfa92f6e422
hal-da <daniel.halasz@gmx.net>
2022-12-01 07:35:10 +0100

    day1

--- day01/day01.js
+++ day01/day01.js
@@ -1,13 +1,11 @@
 import { testData, testResult1,testResult2, realData } from "./day01data.js"
 
-const mySumArr = [];
-
-realData
+const mySumArr = realData
     .split('\n\n')
-    .map(obj => obj.split('\n')
+    .map(arr => arr.split('\n')
         .map(Number)
-    ).forEach(arr =>  mySumArr.push(arr.reduce((a,b)=>a+b)))
+        .reduce((a,b)=>a+b)
+    ).sort((a,b)=>a-b)
 
-const sorted =  mySumArr.sort((a,b)=>a-b)
-console.log(sorted.at(-1));
-console.log(sorted.at(-1) + sorted.at(-2) + sorted.at(-3));
+console.log(mySumArr.at(-1));
+console.log(mySumArr.at(-1) + mySumArr.at(-2) + mySumArr.at(-3));
