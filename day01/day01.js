dc8846dbdbe6a6b97877c8994c55778914b327ba
hal-da <daniel.halasz@gmx.net>
2022-12-01 07:00:09 +0100

    day01

--- day01/day01.js
+++ day01/day01.js
@@ -1,10 +1,13 @@
-import { testData, testResult1,testResult2, realData } from "./day01data.js";
-import { Service } from "../services/service.js"
-const service = new Service()
+import { testData, testResult1,testResult2, realData } from "./day01data.js"
 
-const myfunc1 = () => {
-    const myData = service.getNumArrFromStr(testData)
-    console.log('func1')
-}
+const mySumArr = [];
 
-myfunc1()
+realData
+    .split('\n\n')
+    .map(obj => obj.split('\n')
+        .map(Number)
+    ).forEach(arr =>  mySumArr.push(arr.reduce((a,b)=>a+b)))
+
+    const sorted =  mySumArr.sort((a,b)=>a-b)
+    console.log(sorted.at(-1));
+    console.log(sorted.at(-1) + sorted.at(-2) + sorted.at(-3));
